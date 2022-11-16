94f5f16a0b5ea2b52fe092ce4f7bd5d2cfb5f003
AlapanGhosh10 <alapanzico10@gmail.com>
2022-11-17 01:58:24 +0530

    odd even matrix added

--- odd_even_mat.c
+++ odd_even_mat.c
@@ -1,18 +1,44 @@
 #include<stdio.h>
+#include<math.h>
 int main() {
 	int size;
 	printf("Enter the number of total elements: ");
 	scanf("%d", &size);
-	int odd[size], even[size], in;
+	int arr[size], odd[size], even[size], n, index;
 	int a = 0, b = 0;
+	for(int i = 0 ; i < size ; i++)
+		scanf("%d", &arr[i]);
 	for(int i = 0 ; i < size ; i++) { 
-		scanf("%d", &in);
-		if(in % 2 == 0)
-			even[b++] = in;
+		if(arr[i] % 2 == 0)
+			even[b++] = arr[i];
 		else
-			odd[a++] = in;
+			odd[a++] = arr[i];
 	}
-	int odds = sizeof(odd) / sizeof(int);
-	int evens = sizeof(even) / sizeof(int);
-	printf("%d %d", odds, evens
+	if(a < b)
+		n = a;
+	else
+		n = b;
+	n = sqrt(n);
+	int odds[n][n], evens[n][n];
+	for(int i = 0 ; i < n ; i++) {
+		for(int j = 0 ; j < n ; j++) {
+			index = i * n + j;
+			odds[i][j] =  odd[index];
+			evens[i][j] = even[index];
+		}
+	}
+	printf("\nEVEN MATRIX + ODD MATRIX\n");
+	for(int i = 0 ; i < n ; i++) {
+		for(int j = 0 ; j < n ; j++) 
+			printf("%d ", evens[i][j] + odds[i][j]);
+		printf("\n");
+	}
+	printf("\nEVEN MATRIX - ODD MATRIX\n");
+	for(int i = 0 ; i < n ; i++) {
+		for(int j = 0 ; j < n ; j++)
+			printf("%d ", evens[i][j] - odds[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
 	
