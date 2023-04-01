0785a2babccf3ac2b66ede7df79262427b1fe9ce
AlapanGhosh10 <alapanzico10@gmail.com>
2023-04-01 13:40:12 +0530

    ass1 done

--- DSA/Ass1/sub1_ass1_dsa.c
+++ DSA/Ass1/sub1_ass1_dsa.c
@@ -92,65 +92,6 @@ int *delete(int *arr, int *size, int i) {
 	return arr;
 }
 
-int *merge(int *arr1, int *size1, int *arr2, int *size2) {
-	arr1 = realloc(arr1, (*size1 + *size2) * sizeof(int));
-	int start = *size1;
-	*(size1) = *size1 + *size2;
-	for(int i = 0 ; i < *size2 ; i++) 
-		arr1[start + i] = arr2[i];
-	printf("The array is: ");
-	display(arr1, *size1);
-	return arr1;
-}
-
-int *split(int *arr1, int *size1, int *arr2, int *size2, int i) {
-	if(i < 0 || i > *size1) {
-		printf("Invalid Index\n");
-		return arr2;
-	}
-	arr2 = realloc(arr2, 0 * sizeof(int));
-	*size2 = 0;
-	for(int j = i ; j < *size1 ; j++) {
-		arr2 = realloc(arr2, (*size2 + 1) * sizeof(int));
-		arr2[*size2] = arr1[j];
-		(*size2)++;
-	}
-	arr1 = realloc(arr1, i * sizeof(int));
-	*size1 = i;
-	printf("The first split is: ");
-	display(arr1, *size1);
-	printf("The second split is: ");
-	display(arr2, *size2);
-	return arr2;
-}
-	
-void sort(int *arr, int size) {
-	int i, j, key;
-    	for (i = 1; i < size; i++) {
-        	key = arr[i];
-        	j = i - 1;
-        	while (j >= 0 && arr[j] > key) {
-            		arr[j + 1] = arr[j];
-            		j = j - 1;
-        	}
-        	arr[j + 1] = key;
-    	}
-	printf("The sorted array is: ");
-        for(int i = 0 ; i < size ; i++)
-                printf("%d ", arr[i]);
-        printf("\n");
-}
-
-void search(int *arr, int size, int n) {
-	for(int i = 0 ; i < size ; i++) {
-		if(arr[i] == n) {
-			printf("%d is present at index %d\n", n, i);
-			return;
-		}
-	}
-	printf("%d is not present in the array\n", n);
-}
-
 int main() {
 	int choice;
 	int *arr1 = (int *)calloc(0, sizeof(int));
@@ -166,11 +107,7 @@ int main() {
 		printf("6. Indexed Element\n");
 		printf("7. Insert\n");
 		printf("8. Delete\n");
-		printf("9. Merge\n");
-		printf("10. Split\n");
-		printf("11. Sort\n");
-		printf("12. Search\n");
-		printf("13. Exit\n");
+		printf("9. Exit\n");
 		scanf("%d", &choice);
 		switch(choice) {
 		case 1:
@@ -246,43 +183,6 @@ int main() {
 				delete(arr1, &size1, n);
 			break;
 		case 9:
-			printf("Enter 1 to merge and store in array 1 or 2 to store in array 2: ");
-			scanf("%d", &ch);
-			if(ch == 2)
-				arr2 = merge(arr2, &size2, arr1, &size1);
-			else
-				arr1 = merge(arr1, &size1, arr2, &size2);
-			break;
-		case 10:
-			printf("Enter 1 to split array 1 and 2 to split array 2: ");
-			scanf("%d", &ch);
-			printf("Enter the index to split at: ");
-			scanf("%d", &n);
-			if(ch == 2)
-				arr1 = split(arr2, &size2, arr1, &size1, n);
-			else
-				arr2 = split(arr1, &size1, arr2, &size2, n);
-			break;
-		case 11:
-			printf("Enter 1 to sort array 1 and 2 to sort array 2: ");
-			scanf("%d", &ch);
-			if(ch == 2)
-				sort(arr2, size2);
-			else
-				sort(arr1, size1);
-			break;
-		case 12:
-			printf("Enter 1 to search in array 1 and 2 to search in array 2: ");
-			scanf("%d", &ch);
-			printf("Enter the number: ");
-			scanf("%d", &n);
-			if(ch == 2)
-				search(arr2, size2, n);
-			else
-				search(arr1, size1, n);
-			break;
-			
-		case 13:
 			con = 0;
 			break;
 		default:
